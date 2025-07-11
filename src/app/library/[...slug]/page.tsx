8292e5be31988d248db0c6c13824f28bec4f656f
Typeir <david.t.leiro@gmail.com>
2025-07-11 02:34:36 +0200

    [dirty]: texts

--- src/app/library/[...slug]/page.tsx
+++ src/app/library/[...slug]/page.tsx
@@ -84,7 +84,9 @@ export default async function Page(props: PageProps) {
 
   return (
     <div className='prose prose-invert mx-auto p-10'>
-      <h1>{data.title || slug.join(' / ')}</h1>
+      <h1 className='text-4xl font-mono font-black'>
+        {data.title || slug.join(' / ')}
+      </h1>
       <article
         className={styles.markdown}
         dangerouslySetInnerHTML={{ __html: processed.toString() }}
