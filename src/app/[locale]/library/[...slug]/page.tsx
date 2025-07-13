9894a8e285d0935fa79e8506fe9d27b8ebaa2351
Typeir <david.t.leiro@gmail.com>
2025-07-13 21:31:34 +0200

    [dirty]: resting

--- src/app/[locale]/library/[...slug]/page.tsx
+++ src/app/[locale]/library/[...slug]/page.tsx
@@ -65,7 +65,6 @@ const Page = async ({ params }: PageProps) => {
   }
 
   const { data } = matter(rawContent);
-
   return (
     <div className='prose prose-invert mx-auto p-5'>
       <h1 className='text-4xl font-mono font-black mb-6'>
