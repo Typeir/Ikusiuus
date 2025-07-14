aac8a102757f65ba315fc40d6affbeeac1357ed8
Typeir <david.t.leiro@gmail.com>
2025-07-15 00:43:44 +0200

    [dirty]: fix table overflow

--- src/lib/components/mdx/index.tsx
+++ src/lib/components/mdx/index.tsx
@@ -6,6 +6,9 @@ const components = {
   BlendedImage,
   FlexRenderer,
   ...mdxComponents,
+  table: ({ children }: any) => (
+    <div className='overflow-x-auto max-w-full'>{children}</div>
+  ),
 };
 
 export default components;
