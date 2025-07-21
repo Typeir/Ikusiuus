b1a0b80a1f5ea98c18167ce95140a69a354ff063
Typeir <david.t.leiro@gmail.com>
2025-07-21 15:03:02 +0200

    [dirty]: remove extra theme

--- src/lib/enums/themes.ts
+++ src/lib/enums/themes.ts
@@ -1,7 +1,6 @@
 export enum Theme {
   Light = 'light',
   Dark = 'dark',
-  Damocles = 'damocles',
 }
 
-const THEMES: Theme[] = [Theme.Light, Theme.Dark, Theme.Damocles];
+const THEMES: Theme[] = [Theme.Light, Theme.Dark];
